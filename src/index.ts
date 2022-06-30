774106facffa78bf9112d98a5c4494d1d47d9fcc
Truc Tran <trantrungtruc220691@gmail.com>
2022-07-01 02:12:27 +0700

    Update code

--- src/index.ts
+++ src/index.ts
@@ -35,8 +35,8 @@ app.route("/crawl_empire").get(controller.crawlEmpire);
 app.route("/test").get(controller.test);
 
 cron.schedule('*/5 * * * *', async () => {
-  crawlEmpire.crawlEmpireRange1();
-  // crawlEmpire.crawlEmpireRange2();
+  // crawlEmpire.crawlEmpireRange1();
+  crawlEmpire.crawlEmpireRange2();
   // crawlEmpire.crawlEmpireRange3();
 });
 
