19e5b9107ac0148448da3e2593d9a8393fecd1b9
Truc Tran <trantrungtruc220691@gmail.com>
2022-08-20 22:15:07 +0700

    Update code

--- src/service/mailService.ts
+++ src/service/mailService.ts
@@ -4,16 +4,18 @@ import nodemailer from 'nodemailer';
 export let send = async (subject, content) => {
     try {
         var transporter = nodemailer.createTransport({
-            service: 'gmail',
+            host: 'smtp.gmail.com',
+            port: 465,
             auth: {
-                user: 'crawlgame91@gmail.com',
-                pass: 'Trungtruc'
+                user: 'kyrgiostran@gmail.com',
+                pass: 'kpwqdmgvkjwxobhm'
             }
         });
     
         var mailOptions = {
-            from: 'crawlgame91@gmail.com',
-            to: 'hotrongtin90@gmail.com;hominhtrang2021@gmail.com',
+            from: 'kyrgiostran@gmail.com',
+            to: 'hotrongtin90@gmail.com;hominhtrang2021@gmail.com;trankhanglee001@gmail.com',
+            // to: 'trantrungtruc220691@gmail.com',
             subject: subject,
             text: content
         };
